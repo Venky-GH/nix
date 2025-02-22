45ce5f6890054006367272970bc6bacca178291e
Venkatesh Naidu <naidu.venkateshn@gmail.com>
2025-02-23 01:33:13 +0530

    Pin OneNote to dock

--- flake.nix
+++ flake.nix
@@ -83,6 +83,7 @@
           "/Applications/DaVinci Resolve.app"
           "/Applications/OBS.app"
           "/Applications/Obsidian.app"
+          "/Applications/Microsoft OneNote.app"
         ];
       };
 
